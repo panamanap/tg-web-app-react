db534612863070cc49ffdefbd0ad2de0da092b86
Nazar Polyukhovich <nazar.polyukhovich@gmail.com>
2022-10-04 20:43:57 +0300

    add form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '../Button/Button.jsx';
 import { useTelegram } from '../../hooks/useTelegram';
+import './Header.css';
 
 export const Header = () => {
     const {user, onClose} = useTelegram();
